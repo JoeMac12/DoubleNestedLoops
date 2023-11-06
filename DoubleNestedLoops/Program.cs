5d6bd46559faf7c9eb888c49e72edb1a66e67388
Joseph <josephmacdonald4629@gmail.com>
2023-11-06 08:59:37 -0400

    Initial commit

---
+++ DoubleNestedLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleNestedLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Double nested loops");
+            Console.WriteLine("A loop within a loop");
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey(true);
+        }
+    }
+}
